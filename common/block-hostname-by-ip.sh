d4bc84a160469c67ba50569930b264934797ffa8
Alexander Russkih <oldengremlin@gmail.com>
2017-05-17 16:19:27 +0300

    common

---
+++ common/block-hostname-by-ip.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BORDER=ip.or.router.name
+USER=username
+PASSWORD=password
+
+CP=$( pwd )
+cd /home/olden/src/133-2017/
+./block-hostname-by-ip -r${BORDER} -u${USER} -p${PASSWORD}
+git commit -a -m "reglament run"
+git push origin master
+cd ${CP}
